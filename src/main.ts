203c0c3d51adadf1d3a00b29de10fa544621192b
Cases by Kate <casesbykateNFT@gmail.com>
2021-09-14 18:39:45 +0900

    design

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,66 @@
 import { BodyNode, DomNode, el } from "@hanul/skynode";
+import { utils } from "ethers";
 import MintContract from "./contracts/MintContract";
 import NFTContract from "./contracts/NFTContract";
+import ExtWallet from "./klaytn/ExtWallet";
 import Wallet from "./klaytn/Wallet";
 
 (async () => {
 
+    let balance: DomNode;
     let addressInput: DomNode<HTMLInputElement>;
     let countInput: DomNode<HTMLInputElement>;
 
+    const minted = 10000 - (await NFTContract.balanceOf(MintContract.address)).toNumber();
+
     BodyNode.append(
-        el(".price-info"),
-        el("p", "You can mint your own Cases below.\nGet your own case from <Cases By Kate> and feel free to trace the clues."),
-        el("label",
-            el("span", "Address"),
-            addressInput = el("input", { placeholder: "0x1234..." }),
+        el(".price-info",
+            el("span", "* 20 klay = 1 case\n"),
+            balance = el("span", "* Your Klay balance : "),
         ),
-        el("label",
-            el("span", "Mint Limit"),
-            countInput = el("input"),
+        el("main",
+            el("img", { src: "/images/logo.png", height: "300" }),
+            el("p", "You can mint your own Cases below.\nGet your own case from <Cases By Kate> and feel free to trace the clues."),
+            el("label",
+                el("span", "Address"),
+                addressInput = el("input", { placeholder: "0x1234..." }),
+            ),
+            el("label",
+                el("span", "Mint Limit"),
+                countInput = el("input"),
+            ),
+            el("a.mint-button", "Mint", {
+                click: async () => {
+                    const balance = (await NFTContract.balanceOf(MintContract.address)).toNumber();
+                    const count = parseInt(countInput.domElement.value, 10);
+                    if (balance >= count) {
+                        await MintContract.mint(
+                            addressInput.domElement.value,
+                            count,
+                        );
+                    } else {
+                        alert(`최대 ${balance}개 민팅할 수 있습니다.`);
+                    }
+                },
+            }),
+            el(".progress",
+                "mint progress",
+                el(".meter red", el("span", { style: { width: `${minted / 100}%` } })),
+                el("span", `${minted}/10000 cases`),
+            ),
         ),
-        el("a.mint-button", "Mint", {
-            click: async () => {
-                const balance = (await NFTContract.balanceOf(MintContract.address)).toNumber();
-                const count = parseInt(countInput.domElement.value, 10);
-                if (balance >= count) {
-                    await MintContract.mint(
-                        addressInput.domElement.value,
-                        count,
-                    );
-                } else {
-                    alert(`최대 ${balance}개 민팅할 수 있습니다.`);
-                }
-            },
-        }),
-        el(".meter red", el("span", { style: { width: "80%" } })),
-        el("a.opensea-button"),
-        el("a.site-button"),
+        el("a.opensea-button",
+            el("img", { src: "/images/opensea.png", height: "50" }),
+            { href: "https://opensea.io/collection/cases-by-kate" },
+        ),
+        el("a.website-button",
+            el("img", { src: "/images/website.png", height: "40" }),
+            { href: "https://casesbykate.xyz/" },
+        ),
     );
 
     if (await Wallet.connected() !== true) {
         await Wallet.connect();
     }
+    balance.appendText(utils.formatEther(await ExtWallet.loadBalance()));
 })();
